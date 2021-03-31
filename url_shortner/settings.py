5440badf3f9261e81c1de1d547127b5eafb952d6
shivambhat45 <56789373+shivambhat45@users.noreply.github.com>
2021-03-31 13:06:25 +0530

    hiding secret key

--- url_shortner/settings.py
+++ url_shortner/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rv3rx^(x6i=5sigx11$h4!g5msltn#sc=tc8f)q%y83=_y!yj5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
